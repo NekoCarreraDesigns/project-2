580612ecd1be9dbd8abf50bdd0f898a752bff48f
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-11 10:54:45 -0600

    accepted changes for connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("travelto_db", process.env.DB_USER , process.env.DB_PASS, {
-    host:process.env.DB_HOST,
-    port:3306,
+const sequelize = new Sequelize("travelto_db", "root", "Biggie92#@!*", {
+    host: process.env.DB_HOST,
+    port: 3306,
     dialect: "mysql",
     pool: {
         max: 0,
