4e82b359a97375df949548c0ff40ade8f2046aa0
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-07-18 18:41:59 -0600

    some changes made to travel_controller.

--- controllers/travel_controller.js
+++ controllers/travel_controller.js
@@ -1,43 +1,36 @@
 const express = require("express");
 const router = express.Router();
-
+const uniqid = require("uniqid");
 const db = require("../models");
-const passport = require("passport");
-  
+
+const authenticated = require("../config/middleware/authenticated");
+const passport = require("../config/password");
+// const path = require("path");
     // main homepage 
     router.get("/", (req, res) => {
-
-        db.travelto.findAll({}).then((dbtravelto) => {
-            res.json(dbtravelto)
-        });
-        res.render("index", [travelto])
-
-      console.log("Reached");
-       db.travelto.findAll({}).then((travel) => {
-          console.log(travel);
-           res.json(travel);
-       });
        res.render("index", {style: "style.css"});
     });
-    // user login page after clicking register/login button
-    router.get("/userlogin", (req, res) => {
-         res.render("login", {style: "login.css"});
 
+    // user login page after clicking register/login button
+    router.get("/login", (req, res) => {
+         res.render("login", {style: "login.css"});
     });
 
     //user account page
-    router.get("/users", (req, res) => {
+    router.get("/users", authenticated, (req, res) => {
+        if (req.user){
         db.traveto.findAll({
              where: {
                  first_name: req.body.first_name,
                  last_name: req.body.last_name,
-                 locationsVisited: req.body.locationsVisited,
-                 placesToVisit: req.body.placesToVisit
              }
          }).then((dbtravelto) => {
              res.json(dbtravelto);
          });
-        res.render("user.handlebars", {style: "profile.css"})
+        res.render("user.handlebars", {style: "profile.css"});
+    } else { 
+        res.redirect("/login");
+    }
     });
 
     router.get("/visits", (req, res) => {
@@ -63,7 +56,9 @@ const passport = require("passport");
     });
 
     //locations page, for after searching in homepage
-    router.get("/locations", (req, res) => {
+    router.get("/api/location", (req, res) => {
+        //andrew, get api to send this back. on second thought, this isn't the right query
+        let locationId = req.params.id;
          db.travelto.findAll({
             where: {
                  id: locationId
@@ -75,7 +70,7 @@ const passport = require("passport");
     });
 
 
-    router.post("/new/location", (req, res) => {
+    router.post("/api/location", (req, res) => {
         console.log(req.body);
         db.travelto.create({
             id: req.params.id,
@@ -83,26 +78,40 @@ const passport = require("passport");
             activity: req.body.activity
         });
     });
-    router.post("/login", passport.authenticate("local", { failureRedirect: "/login" }), function (req, res) {
-        res.redirect("/")
-    })
-    router.put("/new/location/:id", (req, res) => {
-        db.travelto.findONe({
+    
+    
+
+    router.put("/location/:id", (req, res) => {
+        db.travelto.findOne({
             where: {
                 id: req.params.id
             }
         });
     });
-    router.delete("/user/location/:id", (req, res) => {
-        db.travelto.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then((dbtravelto) => {
-            res.json(dbtravelto)
-        });
+    
+    router.post("/api/login", passport.authenticate("local", { successRedirect:"/users", failureRedirect:"/login"}), function (req, res) {
+        console.log(req.body);
+        res.json(req.user);
+        res.redirect("")
     });
 
+    router.post("/api/signup", function(req, res){
+        let user = req.body;
+        let id = uniqid();
+        db.Users.create({
+            id: id,
+            username: user.username,
+            first_name: user.firstName,
+            last_name: user.lastName,
+            hashPass: user.pass
+        }).then(function(){
+            res.redirect(200, "/login");
+        }).catch(function(err){
+            console.log(err);
+            res.redirect(500, "/");
+        })
+
+    })
     
 
 module.exports = router;
