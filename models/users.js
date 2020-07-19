f46fcc93a804e0db3c7a95c8fe91f9313e1ac16c
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-07-18 18:29:48 -0600

    db changes

--- models/users.js
+++ models/users.js
@@ -1,41 +1,55 @@
 const bcrypt = require("bcrypt");
 
 module.exports = function (sequelize, DataTypes) {
-    let Users = sequelize.define("Users",{
-        id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        hashPass: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-   
-    Users.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-    };
-  
-  Users.addHook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-      });
-    Users.associate = function(models){
-        Users.hasMany(models.Visits, {
-            foreignKey:true
-        });
+  let Users = sequelize.define(
+    "Users",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      hashPass: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      createdAt: false,
+      timestamps: false,
     }
-    return Users;
-}
+  );
+
+  Users.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  Users.addHook("beforeCreate", function (users) {
+    users.hashPass = bcrypt.hashSync(
+      users.hashPass,
+      bcrypt.genSaltSync(10, "a"),
+      null
+    );
+  });
+
+  Users.associate = function (models) {
+    Users.hasMany(models.Visits, {
+      foreignKey: true,
+      timestamps: false,
+    });
+  };
+  return Users;
+};
