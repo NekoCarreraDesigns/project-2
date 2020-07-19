102455dc18c130457551fa9aa7aefef345d61812
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-07-18 18:27:39 -0600

    changes to config files

--- config/config.js
+++ config/config.js
@@ -1,23 +1,26 @@
 module.exports = {
-    development: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-    },
-    test: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      dialect: 'mysql'
-    },
-    production: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      dialect: 'mysql'
-    }
-  }
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: { freezeTableNames: true, timestamps: false },
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: { freezeTableNames: true, timestamps: false },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: { freezeTableNames: true, timestamps: false },
+  },
+};
