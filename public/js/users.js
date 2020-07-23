fdde274c72686723b9a372b470e7b7d44822d776
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-23 16:01:55 -0600

    further editing of the code

--- public/js/users.js
+++ public/js/users.js
@@ -1,47 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("button.btn#sign-in").on("click", function(e){
-    e.preventDefault();
-    let userD = 
-    {
-        user: $("[name=user]").val().trim(),
-        password: $("[name=pass]").val().trim()
-    };
-    
-    $.ajax("/api/login", {
-        type: "POST",
-        data: userD
-    }).then(()=>{
-        window.location.href = "/users" ;
-    })
-});
+    $("button.btn#sign-in").on("click", function (e) {
+        e.preventDefault();
+        let userD =
+        {
+            user: $("[name=user]").val().trim(),
+            password: $("[name=pass]").val().trim()
+        };
 
-$("button.btn#signup").on("click", function(e){
-    e.preventDefault();
-    let newUser = {
-        username: $("[name=username]").val().trim(),
-        firstName: $("[name=firstname]").val().trim(),
-        lastName: $("[name=lastname]").val().trim(),
-        pass: $("[name=password]").val().trim()
-    };
-    $.ajax("/api/signup", {
-        type: "POST",
-        data: newUser
-    }).then(function(){
-        window.location.href = "/login";
-    })
+        $.ajax("/api/login", {
+            type: "POST",
+            data: userD
+        }).then(() => {
+            location.replace("/user");
+        })
+    });
 
-})
+    $("button.btn#signup").on("click", function (e) {
+        e.preventDefault();
+        let newUser = {
+            username: $("[name=username]").val().trim(),
+            firstName: $("[name=firstname]").val().trim(),
+            lastName: $("[name=lastname]").val().trim(),
+            pass: $("[name=password]").val().trim()
+        };
+        $.ajax("/api/signup", {
+            type: "POST",
+            data: newUser
+        }).then(function () {
+            window.location.href = "/login";
+        })
 
-$("#searchBtn").on("click", function(e){
-    e.preventDefault();
-    let location = $("#loc-search").val().trim();
-
-    $.ajax("/api/location/" + location, {
-        type: "GET"
-    }).then(function(){
-        window.location.href = "/locations"
     })
 
-})
+    $("#searchBtn").on("click", function (e) {
+        e.preventDefault();
+        let location = $("#loc-search").val().trim();
+
+        $.ajax("/api/location/" + location, {
+            type: "GET"
+        }).then(function () {
+            window.location.href = "/locations"
+        })
+
+    })
 })
