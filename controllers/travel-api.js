fe5dc4821850cfb98b5d8be770b84d7a4558b493
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-28 17:25:28 -0600

    continued editing of the app, rewrote the api's, still need to work on the routes

--- controllers/travel-api.js
+++ controllers/travel-api.js
@@ -1,59 +1,134 @@
-$(document).ready(function () {
+let trailName;
+let parkName;
+let routeName;
+let mtnName;
+let currentConditions;
+let searchSummary;
+let searchLocation;
+let difficultyLevel;
 
+function letsGoRunSearch() {
+    let queryRun = "https://www.trailrunproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=200&maxResult=10&key=";
+    let apiKey = process.env.apiKeyRun;
+    let location = $("#location-search").val().trim();
 
-    let queryRun = "https://www.trailrunproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key="
+    $.ajax("/api/locations/:id", {
+        url: queryRun + location + apiKey,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        trailName = response.name;
+        difficultyLevel = response.diffculty;
+        searchSummary = response.summary;
+        searchLocation = response.location;
+        currentConditions = response.conditionDetails;
+        searchedLocationEl = $("#searched-location").text(trailName, difficultyLevel, searchSummary, searchLocation, currentConditions)
+        $("#searched-location").prepend(searchedLocationEl)
 
-
+    });
+}
+function letsGoClimbSearch() {
     let queryClimb = "https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=40.03&lon=-105.25&maxDistance=10&minDiff=5.6&maxDiff=5.10&key="
+    let location = $("#location-search").val().trim();
+    let apiKey = process.env.apiKeyClimb
 
+    $.ajax({
+        url: queryClimb + location + apiKey,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        routeName = response.name;
+        difficultyLevel = response.diffculty;
+        searchSummary = response.summary;
+        searchLocation = response.location;
+        currentConditions = response.conditionDetails;
+        searchedLocationEl = $("#searched-location").text(routeName, difficultyLevel, searchSummary, searchLocation, currentConditions)
+        $("#searched-location").prepend(searchedLocationEl)
+    })
+}
 
+function letsGoMtbSearch() {
     let queryMtb = "https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key="
+    let location = $("#location-search").val().trim();
+    let apiKey = process.env.apiKeyMtb
+
+    $.ajax({
+        url: queryMtb + location + apiKey,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        trailName = response.name;
+        difficultyLevel = response.diffculty;
+        searchSummary = response.summary;
+        searchLocation = response.location;
+        currentConditions = response.conditionDetails;
+        searchedLocationEl = $("#searched-location").text(trailName, difficultyLevel, searchSummary, searchLocation, currentConditions)
+        $("#searched-location").prepend(searchedLocationEl)
+    })
 
 
+}
+
+function letsGoHikingSearch() {
     let queryHike = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key="
+    let location = $("#location-search").val().trim();
+    let apiKey = process.env.apiKeyHike
 
+    $.ajax({
+        url: queryHike + location + apiKey,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        trailName = response.name;
+        difficultyLevel = response.diffculty;
+        searchSummary = response.summary;
+        searchLocation = response.location;
+        currentConditions = response.conditionDetails;
+        searchedLocationEl = $("#searched-location").text(trailName, difficultyLevel, searchSummary, searchLocation, currentConditions)
+        $("#searched-location").prepend(searchedLocationEl)
+    })
 
+}
+
+function letsGoParkSearch() {
     let queryParks = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key="
+    let location = $("#location-search").val().trim();
+    let apiKey = process.env.apiKeyParks
 
+    $.ajax({
+        url: queryParks + location + apiKey,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        parkName = response.name;
+        difficultyLevel = response.diffculty;
+        searchSummary = response.summary;
+        searchLocation = response.location;
+        currentConditions = response.conditionDetails;
+        searchedLocationEl = $("#searched-location").text(parkName, difficultyLevel, searchSummary, searchLocation, currentConditions)
+        $("#searched-location").prepend(searchedLocationEl)
+    })
+}
 
+function letsGoRideSearch() {
     let querySnow = "https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key="
+    let location = $("#location-search").val().trim();
+    let apiKey = process.env.apiKeySnow
+
+    $.ajax({
+        url: querySnow + location + apiKey,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        mtnName = response.name;
+        difficultyLevel = response.diffculty;
+        searchSummary = response.summary;
+        searchLocation = response.location;
+        currentConditions = response.conditionDetails;
+        searchedLocationEl = $("#searched-location").text(mtnName, difficultyLevel, searchSummary, searchLocation, currentConditions)
+        $("#searched-location").prepend(searchedLocationEl)
+    })
+}
 
 
-    $.ajax({
-        url: queryRun + process.env.apiKeyRun,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res);
-    });
-    $.ajax({
-        url: queryClimb + process.env.apiKeyClimb,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res);
-    });
-    $.ajax({
-        url: queryHike + process.env.apiKeyHike,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res);
-    });
-    $.ajax({
-        url: queryParks + process.env.apiKeyParks,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res);
-    });
-    $.ajax({
-        url: queryMtb + process.env.apiKeyMtb,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res);
-    });
-    $.ajax({
-        url: querySnow + process.env.apiKeySnow,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res);
-    });
 
-});
